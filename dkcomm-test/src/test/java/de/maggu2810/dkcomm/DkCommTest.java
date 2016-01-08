2a5af4b2cf46cf9a4438221a3e92feca16c84bfa
Markus Rathgeb <maggu2810@gmail.com>
2016-01-08 14:56:37 +0100

    add working pax exam test
    Signed-off-by: Markus Rathgeb <maggu2810@gmail.com>

--- dkcomm-test/src/test/java/de/maggu2810/dkcomm/DkCommTest.java
+++ dkcomm-test/src/test/java/de/maggu2810/dkcomm/DkCommTest.java
@@ -1,19 +1,13 @@
 package de.maggu2810.dkcomm;
 
-import static org.ops4j.pax.exam.CoreOptions.maven;
-import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
-import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.configureConsole;
-import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.features;
-import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.karafDistributionConfiguration;
-import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.keepRuntimeFolder;
+import static org.ops4j.pax.exam.CoreOptions.*;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.*;
 
 import java.io.File;
 import java.util.Enumeration;
+
 import javax.comm.CommPortIdentifier;
 
-import javax.inject.Inject;
-
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.Configuration;
@@ -28,47 +22,51 @@ import org.slf4j.LoggerFactory;
 @RunWith(PaxExam.class)
 public class DkCommTest {
 
-  private final static Logger LOG = LoggerFactory.getLogger(DkCommTest.class);
+    private final static String DFL_KARAF_VERSION = "4.0.3";
 
-  @Configuration
-  public Option[] config() {
+    private final static Logger LOG = LoggerFactory.getLogger(DkCommTest.class);
 
-    MavenArtifactUrlReference karafUrl = maven()
-            .groupId("org.apache.karaf")
-            .artifactId("apache-karaf")
-            .version(karafVersion())
-            .type("zip");
+    @Configuration
+    public Option[] config() {
 
-    MavenUrlReference karafStandardRepo = maven()
-            .groupId("org.apache.karaf.features")
-            .artifactId("standard")
-            .version(karafVersion())
-            .classifier("features")
-            .type("xml");
+        MavenArtifactUrlReference karafUrl = maven().groupId("org.apache.karaf").artifactId("apache-karaf")
+                .version(karafVersion()).type("zip");
 
-    return new Option[]{
-      // KarafDistributionOption.debugConfiguration("5005", true),
-      karafDistributionConfiguration().frameworkUrl(karafUrl).unpackDirectory(new File("target", "exam")).useDeployFolder(false),
-      keepRuntimeFolder(),
-      configureConsole().ignoreLocalConsole(),
-      features(karafStandardRepo, "scr"),
-      mavenBundle().groupId("de.maggu2810.dkcomm").artifactId("dkcomm").versionAsInProject().start()};
-  }
+        MavenUrlReference karafStandardRepo = maven().groupId("org.apache.karaf.features").artifactId("standard")
+                .version(karafVersion()).classifier("features").type("xml");
 
-  public static String karafVersion() {
-    ConfigurationManager cm = new ConfigurationManager();
-    String karafVersion = cm.getProperty("pax.exam.karaf.version", "4.0.3");
-    return karafVersion;
-  }
-
-  @Test
-  public void testIteration() {
-    Enumeration<CommPortIdentifier> portList = CommPortIdentifier.getPortIdentifiers();
-
-    while (portList.hasMoreElements()) {
-      CommPortIdentifier portId = portList.nextElement();
-      LOG.info("port: type={}, name={}", portId.getPortType(), portId.getName());
+        return new Option[] {
+                // enable for debugging
+                // KarafDistributionOption.debugConfiguration("5005", true),
+                // setup karaf distribution
+                karafDistributionConfiguration().frameworkUrl(karafUrl).unpackDirectory(new File("target", "exam"))
+                        .useDeployFolder(false),
+                //
+                keepRuntimeFolder(),
+                //
+                configureConsole().ignoreLocalConsole(),
+                // let's add some features
+                features(karafStandardRepo, "scr"),
+                // add the dkcomm bundle
+                mavenBundle().groupId("de.maggu2810.dkcomm").artifactId("dkcomm").versionAsInProject().start()
+                // add additional options here...
+        };
     }
-  }
+
+    public static String karafVersion() {
+        ConfigurationManager cm = new ConfigurationManager();
+        String karafVersion = cm.getProperty("pax.exam.karaf.version", DFL_KARAF_VERSION);
+        return karafVersion;
+    }
+
+    @Test
+    public void testIteration() {
+        Enumeration<CommPortIdentifier> portList = CommPortIdentifier.getPortIdentifiers();
+
+        while (portList.hasMoreElements()) {
+            CommPortIdentifier portId = portList.nextElement();
+            LOG.info("port: type={}, name={}", portId.getPortType(), portId.getName());
+        }
+    }
 
 }
