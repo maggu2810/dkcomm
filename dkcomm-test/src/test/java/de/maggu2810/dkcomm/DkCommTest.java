b32a19c99d3377ae3bd21d66e3f1d0ec8f33059b
Markus Rathgeb <maggu2810@gmail.com>
2016-01-08 15:59:57 +0100

    test: add open port example (error handling)
    Signed-off-by: Markus Rathgeb <maggu2810@gmail.com>

--- dkcomm-test/src/test/java/de/maggu2810/dkcomm/DkCommTest.java
+++ dkcomm-test/src/test/java/de/maggu2810/dkcomm/DkCommTest.java
@@ -6,7 +6,12 @@ import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.*;
 import java.io.File;
 import java.util.Enumeration;
 
+import javax.comm.CommPort;
 import javax.comm.CommPortIdentifier;
+import javax.comm.NoSuchPortException;
+import javax.comm.ParallelPort;
+import javax.comm.PortInUseException;
+import javax.comm.SerialPort;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,6 +19,7 @@ import org.ops4j.pax.exam.Configuration;
 import org.ops4j.pax.exam.ConfigurationManager;
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.karaf.options.LogLevelOption.LogLevel;
 import org.ops4j.pax.exam.options.MavenArtifactUrlReference;
 import org.ops4j.pax.exam.options.MavenUrlReference;
 import org.slf4j.Logger;
@@ -41,12 +47,14 @@ public class DkCommTest {
                 // setup karaf distribution
                 karafDistributionConfiguration().frameworkUrl(karafUrl).unpackDirectory(new File("target", "exam"))
                         .useDeployFolder(false),
+                // Set log level
+                logLevel(LogLevel.INFO),
                 //
                 keepRuntimeFolder(),
                 //
                 configureConsole().ignoreLocalConsole(),
                 // let's add some features
-                features(karafStandardRepo, "scr"),
+                features(karafStandardRepo, "logging", "scr"),
                 // add the dkcomm bundle
                 mavenBundle().groupId("de.maggu2810.dkcomm").artifactId("dkcomm").versionAsInProject().start()
                 // add additional options here...
@@ -66,6 +74,44 @@ public class DkCommTest {
         while (portList.hasMoreElements()) {
             CommPortIdentifier portId = portList.nextElement();
             LOG.info("port: type={}, name={}", portId.getPortType(), portId.getName());
+        }
+    }
+
+    @Test
+    public void testOpenPort() {
+        for (final String p : new String[] { "/dev/ttyUSB1", "/dev/ttyUSB0", "/dev/ttyUSB2" }) {
+            final CommPortIdentifier portIdent;
+            LOG.info("Get port identifier ({})", p);
+            try {
+                portIdent = CommPortIdentifier.getPortIdentifier(p);
+            } catch (NoSuchPortException e) {
+                LOG.warn("failure: {}; open failed (no such port)", p);
+                continue;
+            }
+
+            final CommPort port;
+            LOG.info("Open port ({})", p);
+            try {
+                port = portIdent.open("myApp", 1000);
+            } catch (PortInUseException e) {
+                LOG.warn("failure: {}; port is already in use", p);
+                continue;
+            }
+            if (port == null) {
+                LOG.warn("failure: {}; open port returned null; perhaps insufficient rights for device", p);
+                continue;
+            }
+
+            final String portType;
+            if (port instanceof SerialPort) {
+                portType = "serial";
+            } else if (port instanceof ParallelPort) {
+                portType = "parallel";
+            } else {
+                portType = "unknown";
+            }
+            LOG.info("opened port: name={}, type={}", port.getName(), portType);
+            port.close();
         }
     }
 
